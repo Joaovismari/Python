57b04024768d798c77f1330cfe7c59401446c9de
João Marcos Vismari de Lacerda <jmvismari@hotmail.com>
2021-06-01 12:40:30 -0300

    introdução e implementação de tuplas como coleção em python

---
+++ colecoes/tuplas.py
@@ -0,0 +1,24 @@
+#Tuplas
+
+'''
+se a tupla tiver apenas um elemento, é necessario acrescentar uma virgula
+após este elemento para diferenciar de uma String
+'''
+
+
+
+times_rj = ('Botafogo', 'Flamengo', 'Fluminense', 'Vasco')
+print(type(times_rj)) # class=’tuple’
+print(times_rj) # ('Botafogo', 'Flamengo', 'Fluminense', 'Vasco')
+
+
+objeto_string = ('tesoura')
+objeto_tupla = ('tesoura',)
+
+print(type(objeto_string)) # class 'str'
+print(type(objeto_tupla)) # class 'tuple'
+
+print(times_rj[1])
+
+
+print(objeto_string)
