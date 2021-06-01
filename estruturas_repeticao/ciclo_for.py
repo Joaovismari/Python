88e8047d5ce45d11e5f6081ae2d9814b6fab174f
João Marcos Vismari de Lacerda <jmvismari@hotmail.com>
2021-06-01 12:36:22 -0300

    demonstração pratica de um ciclo de repetição for

---
+++ estruturas_repeticao/ciclo_for.py
@@ -0,0 +1,12 @@
+nomes = ['Pedro', 'João', 'Leticia']
+for n in nomes: #passa por todos os elementos da lista atribuindo seus valores a variavel n
+    print(n)#imprime o valor de n em uma linha
+else:
+    print("Todos os nomes foram listados com sucesso")#após todas a posições terem sido utilizadas a cima
+
+print('_____________________________________________')
+
+meses = ['janeiro', 'fevereiro', 'março', 'abril']
+for mes in meses:
+    print('mês de', mes)
+
